08a0ea501d03136b4bb27cb7acbe15323c883c39
Jonathan Thimesch <JRThimesch@gmail.com>
2019-10-07 23:09:59 -0500

    Add files via upload

--- ParseRewrite.py
+++ ParseRewrite.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import re
 
 HEADER_STR = 'Incidents with Offenses / Warrants'
 HEADER_OFFSET = len('Incidents with Offenses / Warrants')
@@ -12,15 +13,40 @@ def getPageRange(_pdfObj):
     numOfPages = _pdfObj.getNumPages()
     return range(numOfPages)
 
-def getTrimmedPageText(_pageText):
-    pass
+def regexSplitandJoin(_str, _pattern):
+    return ' '.join(re.split(_pattern, _str))
 
-with open('text.txt', 'w') as file:
-    pdfObject = openPDF('PDFs/08-12-19.pdf')
-    for page in getPageRange(pdfObject):
-        pageTextFull = pdfObject.getPage(page).extractText()
-        headerTrimIndex = pageTextFull.find(HEADER_STR)
-        headerTrimIndex += HEADER_OFFSET
-        pageTextNoHeader = pageTextFull[headerTrimIndex:]
-        file.write(pageTextNoHeader)
-        print(pageTextNoHeader)
+def getTrimmedPageText(_page):
+    pageTextFull = pdfObject.getPage(page).extractText()
+
+    # Header is trimmed from the string defined at the top
+    # An offset is added since the find function finds the beginning index
+    headerTrimIndex = pageTextFull.find(HEADER_STR)
+    headerTrimIndex += HEADER_OFFSET
+    pageTextTrimmed = pageTextFull[headerTrimIndex:]
+
+    # Last page contains a footer that must be removed
+    # Last page is defined by the presence of ###Total Arrests:
+    if 'Arrests:' in pageTextTrimmed:
+        pageTextTrimmedList = pageTextTrimmed.split()
+        pageTextTrimmedList = pageTextTrimmedList[:-2]
+        pageTextTrimmed = ' '.join(pageTextTrimmedList)
+
+    return pageTextTrimmed
+
+def fixTextSpacing(_fullText):
+    _fullText = regexSplitandJoin(_fullText, '(\d{2}/\d{2}/\d{4})')
+    _fullText = regexSplitandJoin(_fullText, '(\d{2}:\d{2})')
+    _fullText = _fullText.replace('Sedgwick County Warrant', ' Sedgwick County Warrant ')
+    return _fullText
+
+if __name__ == "__main__":
+    with open('text.txt', 'w') as file:
+        pdfObject = openPDF('PDFs/08-12-19.pdf')
+        fullText = ''
+        for page in getPageRange(pdfObject):
+            pageText = getTrimmedPageText(page)
+            pageTextSpaced = fixTextSpacing(pageText)
+            fullText += pageTextSpaced + ' '
+        file.write(fullText)
+        print(fullText)
