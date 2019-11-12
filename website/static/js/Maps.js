5711864781061ed3688bf05ffa93d9a5f7dfe86c
Jonathan Thimesch <JRThimesch@gmail.com>
2019-11-12 10:36:35 -0600

    Initial rewrite commit.

--- website/static/js/Maps.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GoogleMap from "./GoogleMap";
-
-ReactDOM.render(
-    <GoogleMap center={{lat: 37.6872, lng: -97.3301}} zoom={13} />, 
-    document.getElementById('leaflet-map')
-);
