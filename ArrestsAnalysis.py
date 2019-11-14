013f452b1ffe44e835383e9fd06c356506c90c98
Jonathan Thimesch <JRThimesch@gmail.com>
2019-11-13 23:48:05 -0600

    fixed some taggings, expanded on analysis of arrests

--- ArrestsAnalysis.py
+++ ArrestsAnalysis.py
@@ -1,33 +1,17 @@
-"""     def getSeparatedArrests(_dataframe):
-    listOfArrests = _dataframe['Arrests'].tolist()
-    for arrests in listOfArrests:
-        for arrest in arrests:
-            yield arrest
-
-    
-    arrests = [arrest for arrest in arrestsSeries.tolist() if arrest != 'No arrests listed.']
-    tags = getTagsFromArrests(arrests)
-    #stemmedArrests = [getStemmedAndTokenizedArrest(arrest) for arrest in arrests]
-    #dataDict = {'arrests' : arrests, 'tags' : tags, 'stemmed' : stemmedArrests}
-    uniqueArrests = pd.Series(arrestsSeries.unique()).tolist()
-    uniqueStems = map(getStemmedAndTokenizedArrest, uniqueArrests)
-    uniqueTags = getTagsFromArrests(uniqueArrests)
-    uniques = pd.DataFrame(data=[uniqueArrests, uniqueTags, uniqueStems], columns=['arrest', 'tag', 'stem'])
-    print(uniques)
-    #learningData = pd.DataFrame(data=dataDict)
-    #learningData.to_csv('data/tagData.csv', sep = '|')
-    learningDataCounts = pd.DataFrame(data=pd.Series(tags).value_counts().sort_index())
-
-    print(learningDataCounts) """
-
 import os
 import pandas as pd
+from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 def getCSVs(_path):
     files = os.listdir(_path)
     return [_path + file for file in files if file.endswith('.csv')]
 
 def getFullDataFrame(_CSVs):
+    strToList = lambda x: x.strip("[]").replace("\"", "'").split("', ")
+    conversionDict = {"Arrests" : strToList,
+        "Tags" : strToList}
     fullDataframe = pd.DataFrame(data=[])
     for csv in _CSVs:
         print('Opening...', csv)
@@ -35,14 +19,48 @@ def getFullDataFrame(_CSVs):
         fullDataframe = fullDataframe.append(dfCSV, ignore_index=True)
     return fullDataframe
 
+def getArrestsFromColumn(_column):
+    arrests = []
+    for listOfArrests in _column:
+        for arrest in listOfArrests:
+            arrest = arrest.strip("'")
+            if arrest != "No arrests listed.":
+                arrests.append(arrest)
+    return arrests
+
+def getTagsFromColumn(_column):
+    tags = []
+    for listOfTags in _column:
+        tags += [tag.strip("'") for tag in listOfTags if tag]
+    return tags
+
+def getStemmedAndTokenizedArrest(_arrest):
+    stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'\w+')
+
+    _arrest = _arrest.lower()
+    _arrest = _arrest.replace(':', '')
+    _arrest = _arrest.replace('/', ' ')
+
+    tokens = tokenizer.tokenize(_arrest)
+    filteredWords = filter(lambda token: token not in stopwords.words('english'), tokens)
+    stemmedArrest = map(stemmer.stem, filteredWords)
+    tokenizedAndStemmedArrest = ' '.join(stemmedArrest)
+    return tokenizedAndStemmedArrest
+
 if __name__ == "__main__":
-    conversionDict = {"Arrests": lambda x: x.strip("[]").split("', ")}
     CSVs = getCSVs('CSVs/')
     
     dfFull = getFullDataFrame(CSVs)
+    arrestsFull = dfFull['Arrests']
+    tagsFull = dfFull['Tags']
+
+    arrests = getArrestsFromColumn(arrestsFull)
+    tags = getTagsFromColumn(tagsFull)
+    stemmedArrests = map(getStemmedAndTokenizedArrest, arrests)
+
+    dataDict = {'arrests' : arrests, 'tags' : tags, 'stemmed' : stemmedArrests}
     
-    arrestsFull = dfFull['Arrests'] 
-    #print(arrestsFull)
-    for listOfArrests in arrestsFull:
-        for arrest in listOfArrests:
-            print(arrest.strip("'\""))
+    trainingData = pd.DataFrame(data=dataDict)
+    trainingData.to_csv('data/tagData.csv', sep = '|')
+    print(trainingData)
